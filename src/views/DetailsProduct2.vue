777431009636e077829b8ec4e9b1e8b7cdc2db38
JonasDeBruyne <jonasdebruyne734@gmail.com>
2024-11-17 20:00:23 +0100

    feat: icons updated

--- src/views/DetailsProduct2.vue
+++ src/views/DetailsProduct2.vue
@@ -6,8 +6,8 @@ import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
-const lacesColors = ref<string[]>([]); // Kleuren voor de veters
-const solesColors = ref<string[]>([]); // Kleuren voor de zolen
+const lacesColors = ref<string[]>([]);
+const solesColors = ref<string[]>([]);
 
 const route = useRoute();
 const productCode = ref<string | undefined>(undefined);
@@ -65,9 +65,15 @@ onMounted(() => {
   gltfLoader.load(
     "/models/Shoe_compressed.glb",
     (gltf) => {
+      // Log de gehele scene van het GLB model
+      console.log("Loaded GLB model scene:", gltf.scene);
+
       gltf.scene.scale.set(50, 50, 50);
       gltf.scene.position.set(0, 0, 0);
       scene.add(gltf.scene);
+
+      // Log alle kinderen van de scène (mogelijk de verschillende lagen of objecten)
+      console.log("Children in GLB scene:", gltf.scene.children);
 
       const controls = new OrbitControls(camera, renderer.domElement);
       controls.enableDamping = true;
@@ -246,6 +252,52 @@ onMounted(() => {
   <div class="container">
     <h3 class="logo">REBILT</h3>
     <div class="model"></div>
+    <div class="icons">
+      <router-link :to="`/`">
+        <div class="icon">
+          <div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="26"
+              height="26"
+              viewBox="0 0 256 256"
+            >
+              <path
+                d="M104,216V152h48v64h64V120a8,8,0,0,0-2.34-5.66l-80-80a8,8,0,0,0-11.32,0l-80,80A8,8,0,0,0,40,120v96Z"
+                fill="none"
+                stroke="white"
+                stroke-width="8"
+              ></path>
+            </svg>
+          </div>
+          <p>Home</p>
+        </div>
+      </router-link>
+      <div class="icon">
+        <div>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+            <path
+              d="M11.98 2.36a.75.75 0 00-.33.1L8.3 4.11H8.3a.08.08 0 00-.01.01.76.76 0 00-.31 1c.09.18.24.31.43.38.19.06.4.05.57-.04l2.26-1.13v2.7c0 .2.08.39.22.53a.76.76 0 001.08 0 .76.76 0 00.22-.54V4.34L15 5.47h.01a.76.76 0 001.03-.91.75.75 0 00-.34-.42l-.01-.01a.08.08 0 00-.01 0l-3.34-1.68a.75.75 0 00-.37-.09zm-5.57 2.8a.76.76 0 00-.34.08L2.72 6.9a.76.76 0 00-.42.7v3.9c0 .2.08.39.22.53a.76.76 0 001.07 0 .76.76 0 00.22-.54V8.81l2.26 1.13a.76.76 0 001.05-.91.76.76 0 00-.35-.42.08.08 0 00-.02-.02l-2-1 2-1c.3-.16.48-.52.4-.85a.77.77 0 00-.74-.58zm11.18 0a.77.77 0 00-.73.58c-.08.32.08.67.37.83l.02.02 2 1-2 1a.08.08 0 00-.02.02c-.17.1-.3.24-.35.42a.76.76 0 00.47.95c.19.06.4.05.58-.04l2.26-1.13v2.7c0 .2.08.39.22.53a.76.76 0 001.07 0 .76.76 0 00.22-.54V7.6a.76.76 0 00-.42-.69l-3.35-1.67a.76.76 0 00-.34-.08zm-2.24 4.47a.75.75 0 00-.33.08L12 11.2l-3.02-1.5a.76.76 0 00-1.05.91c.06.18.2.33.36.42a.08.08 0 000 .01h.01a.08.08 0 000 .01l2.94 1.47v2.89c0 .2.08.39.22.53a.76.76 0 001.08 0 .76.76 0 00.22-.53v-2.9l2.93-1.46.02-.02c.3-.16.45-.5.37-.83a.77.77 0 00-.73-.58zM3.01 14.11a.75.75 0 00-.7.75v3.9c-.01.29.15.56.41.69l3.35 1.67a.76.76 0 001.05-.91.76.76 0 00-.34-.42.08.08 0 00-.03-.02l-2-1 2-1 .02-.02c.17-.1.3-.24.35-.42a.76.76 0 00-.47-.95.77.77 0 00-.58.04l-2.26 1.13v-2.7c0-.2-.08-.4-.24-.55a.76.76 0 00-.56-.2zm17.88 0a.76.76 0 00-.7.75v2.69l-2.26-1.13a.76.76 0 00-1.05.91c.06.18.18.33.35.42a.08.08 0 00.01 0v.02a.08.08 0 00.01 0l2 1-2 1h-.01a.08.08 0 000 .01h-.01a.76.76 0 00.13 1.37c.18.07.39.06.57-.03l3.35-1.67c.26-.13.42-.4.42-.69v-3.9a.76.76 0 00-.8-.75zm-8.94 4.47a.08.08 0 00-.03 0 .75.75 0 00-.47.23.76.76 0 00-.2.52v2.69l-2.26-1.13a.08.08 0 00-.01 0 .76.76 0 00-1.03.91c.05.18.18.33.34.42l.01.01a.08.08 0 00.01.01l3.34 1.67c.22.12.49.11.7 0l3.34-1.67.02-.01a.76.76 0 00.31-1 .76.76 0 00-1-.34l-2.26 1.13v-2.7a.76.76 0 00-.8-.74z"
+            ></path>
+          </svg>
+        </div>
+        <p>AR</p>
+      </div>
+    </div>
+    <div class="rotate-informer">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        fill="#ffffff"
+        viewBox="0 0 256 256"
+      >
+        <path
+          d="M232,48V88a8,8,0,0,1-16,0V56H184a8,8,0,0,1,0-16h40A8,8,0,0,1,232,48ZM72,200H40V168a8,8,0,0,0-16,0v40a8,8,0,0,0,8,8H72a8,8,0,0,0,0-16Zm152-40a8,8,0,0,0-8,8v32H184a8,8,0,0,0,0,16h40a8,8,0,0,0,8-8V168A8,8,0,0,0,224,160ZM32,96a8,8,0,0,0,8-8V56H72a8,8,0,0,0,0-16H32a8,8,0,0,0-8,8V88A8,8,0,0,0,32,96ZM188,167l-56,32a8,8,0,0,1-7.94,0L68,167A8,8,0,0,1,64,160V96a8,8,0,0,1,4-7l56-32a8,8,0,0,1,7.94,0l56,32a8,8,0,0,1,4,7v64A8,8,0,0,1,188,167ZM88.12,96,128,118.79,167.88,96,128,73.21ZM80,155.36l40,22.85V132.64L80,109.79Zm96,0V109.79l-40,22.85v45.57Z"
+        ></path>
+      </svg>
+      <p>Drag to rotate</p>
+    </div>
     <div class="config-wrapper">
       <div class="links">
         <a href="#" class="overviewButton visibility">
@@ -387,6 +439,83 @@ onMounted(() => {
   position: relative;
 }
 
+.rotate-informer {
+  position: absolute;
+  top: 55%;
+  left: calc(75% / 2 - 12px);
+  z-index: 999;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 8px;
+  padding: 12px 16px;
+  border-radius: 4px;
+  background-color: #1a1a1a;
+  box-shadow: 0 0 8px #0006;
+  transition: transform 0.5s;
+}
+
+.icons {
+  position: absolute;
+  top: 48%;
+  left: calc(75% / 2 - 12px);
+  z-index: 999;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  gap: 24px;
+}
+
+.icons .icon {
+  display: flex;
+  flex-direction: row;
+  gap: 8px;
+  align-items: center;
+}
+
+.icons .icon div {
+  border-radius: 100%;
+  background-color: #1a1a1a;
+  box-shadow: 0 0 8px #0006;
+  transition: transform 0.5s;
+  width: 40px;
+  height: 40px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+}
+
+.icons .icon p {
+  background-color: #1a1a1a;
+  color: #fff;
+  padding: 8px 12px;
+  border-radius: 4px;
+  font-size: 14px;
+  line-height: 14px;
+  margin: 0;
+  pointer-events: none;
+  transition: opacity 0.4s ease;
+  letter-spacing: 1px;
+  opacity: 0;
+  transition: opacity 0.3s ease;
+}
+
+.icons .icon:hover p {
+  opacity: 1;
+}
+
+.icons div svg {
+  width: 24px;
+  height: 24px;
+  fill: #fff;
+}
+
+.rotate-informer p {
+  color: var(--white);
+}
+
 .config-wrapper {
   display: flex;
   flex-direction: column;
@@ -512,6 +641,17 @@ li svg {
   .model {
     width: 75%;
     height: 100%;
+  }
+
+  .icons {
+    top: 50%;
+    left: 24px;
+    flex-direction: column;
+    align-items: flex-start;
+  }
+
+  .rotate-informer {
+    top: 94%;
   }
 
   .config-wrapper {
