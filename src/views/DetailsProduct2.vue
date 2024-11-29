7ee016d14909c30c9d5b23bb1cacdf4cfb6d551e
JonasDeBruyne <jonasdebruyne734@gmail.com>
2024-11-29 16:10:27 +0100

    feat: links removed

--- src/views/DetailsProduct2.vue
+++ src/views/DetailsProduct2.vue
@@ -595,28 +595,6 @@ onMounted(() => {
       <p>Drag to rotate</p>
     </div>
     <div class="config-wrapper">
-      <div class="links">
-        <a href="#" class="overviewButton visibility">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 320 512"
-            style="transform: rotate(180deg)"
-          >
-            <path
-              d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z"
-            ></path>
-          </svg>
-          <p>Overview</p>
-        </a>
-        <a href="#" class="summaryButton">
-          <p>Summary</p>
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
-            <path
-              d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z"
-            ></path>
-          </svg>
-        </a>
-      </div>
       <div class="elements">
         <div class="overview">
           <h2>Overview</h2>
