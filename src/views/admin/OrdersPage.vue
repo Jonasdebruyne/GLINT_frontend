2e577eab3123eba531f1bafc4ddb14f0747b0a12
JonasDeBruyne <jonasdebruyne734@gmail.com>
2024-12-01 21:41:10 +0100

    feat: order updated

--- src/views/admin/OrdersPage.vue
+++ src/views/admin/OrdersPage.vue
@@ -106,6 +106,7 @@ const fetchPartnerData = async () => {
 const fetchOrders = async () => {
   const ordersData = await fetchDataFromApi(`${baseURL}/orders`);
   orders.value = ordersData?.orders || [];
+  console.log(orders.value);
 };
 
 // Initial Fetch on Mount
